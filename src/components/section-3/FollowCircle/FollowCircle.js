5aa8fdc936082bd83b1621412683e9002eed04e5
Maya Jerndahl <144351614+MjBonk@users.noreply.github.com>
2024-02-11 18:36:34 +0100

    follow circle refractoring

--- src/components/section-3/FollowCircle/FollowCircle.js
+++ src/components/section-3/FollowCircle/FollowCircle.js
@@ -1,18 +1,43 @@
 import { motion } from "framer-motion";
-import "./Circle.css"
-import { useRef } from "react";
-import { useFollowPointer } from "./UseFollowPointer.js";
+import styles from "./FollowCircle.module.css";
+import { useState, useRef, useEffect } from "react";
 
 function FollowCircle() {
+	const [point, setPoint] = useState({ x: 0, y: 0 });
 	const ref = useRef(null);
-	const { x, y } = useFollowPointer(ref);
+
+	useEffect(() => {
+		if (!ref.current) return;
+		const handlePointerMove = ({ clientX, clientY }) => {
+			const childEl = ref.current;
+			const parentEl = childEl.parentElement;
+			const x = Math.max(
+				0,
+				Math.min(
+					clientX - parentEl.offsetLeft - childEl.offsetWidth / 2,
+					parentEl.offsetWidth - childEl.offsetWidth
+				)
+			);
+			const y = Math.max(
+				0,
+				Math.min(
+					clientY - parentEl.offsetTop - childEl.offsetHeight / 2,
+					parentEl.offsetHeight - childEl.offsetHeight
+				)
+			);
+			setPoint({ x, y });
+		};
+		window.addEventListener("pointermove", handlePointerMove);
+		return () => window.removeEventListener("pointermove", handlePointerMove);
+	}, [ref]);
+
 	return (
-		<div className="flex">
-			<div className="circle">
+		<div className={styles.wrapper}>
+			<div className={styles.circle}>
 				<motion.div
 					ref={ref}
-					className="box"
-					animate={{ x, y }}
+					className={styles.ball}
+					animate={{ x: point.x, y: point.y }}
 					transition={{
 						type: "spring",
 						damping: 3,
